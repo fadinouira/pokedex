c242ea52091d10036b71d410e3e94dd525fc07d2
Fadi Nouira <fadi.nouira1@gmail.com>
2025-02-24 00:03:46 +0100

    feat: implement SearchForm component with form handling and styling

--- src/app/features/pokemon/components/SearchForm/SearchForm.tsx
+++ src/app/features/pokemon/components/SearchForm/SearchForm.tsx
@@ -1,1 +1,36 @@
-export function SearchForm() {}
+import { Button, Input, Typography } from '@/shared';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { SearchFormProps, SearchFormType } from './SearchForm.type';
+
+import './SearchForm.css';
+
+export function SearchForm({ isLoading, onSubmit }: SearchFormProps) {
+  const { register, handleSubmit } = useForm<SearchFormType>();
+  const { t } = useTranslation();
+
+  const submitHandler: SubmitHandler<SearchFormType> = (data) => {
+    onSubmit(data);
+  };
+
+  return (
+    <form className="search-form-container" onSubmit={handleSubmit(submitHandler)}>
+      <Typography
+        className="search-form-label"
+        font="secondary"
+        fontSize="sm"
+        lineHeight="sm"
+        fontWeight="bold"
+        color="text-label"
+      >
+        {t('pokemon.name_or_id')}
+      </Typography>
+      <Input {...register('pokemonIdentifier')} disabled={isLoading} inputSize={'large'} />
+
+      <div className="search-form-actions">
+        <Button label={t('common.search')} type="submit" disabled={isLoading} />
+        <Button label={t('common.random')} disabled={isLoading} />
+      </div>
+    </form>
+  );
+}
--- src/locales/en/pokemon.ts
+++ src/locales/en/pokemon.ts
@@ -1,3 +1,4 @@
 export const pokemon = {
   not_found: 'No Pokemon Found!',
+  name_or_id: 'Pokemon Name or Id',
 };
