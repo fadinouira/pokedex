e0f1c91c7f85191bfa736d7368369948e6817c54
Fadi Nouira <fadi.nouira1@gmail.com>
2025-02-26 16:28:47 +0100

    fix: Remove 'pokemon.moves' tab from PokemonCard constants

--- src/app/features/pokemon/components/PokemonCard/PokemonCard.constants.ts
+++ src/app/features/pokemon/components/PokemonCard/PokemonCard.constants.ts
@@ -3,5 +3,4 @@ import { TabItem } from '@/shared';
 export const POKEMON_CARD_TABS: TabItem[] = [
   { label: 'pokemon.stats' },
   { label: 'pokemon.evolutions' },
-  { label: 'pokemon.moves' },
 ];
