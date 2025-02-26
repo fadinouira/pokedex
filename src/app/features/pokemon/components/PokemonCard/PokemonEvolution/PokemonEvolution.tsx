fb415eaf30ab4987ee82756f4d624ec821605f68
Fadi Nouira <fadi.nouira1@gmail.com>
2025-02-26 21:28:04 +0100

    feat: Update PokemonEvolution component styling and improve layout for evolution items

--- src/app/features/pokemon/components/PokemonCard/PokemonEvolution/PokemonEvolution.tsx
+++ src/app/features/pokemon/components/PokemonCard/PokemonEvolution/PokemonEvolution.tsx
@@ -1,23 +1,28 @@
 import { generateStyleToken, Typography } from '@/shared';
+import LongArrowRight from '@/shared/assets/icons/long_arrow_right.svg?react';
 import { Fragment } from 'react/jsx-runtime';
 import './PokemonEvolution.css';
 import { PokemonEvolutionProps } from './PokemonEvolution.type';
-import LongArrowRight from '@/shared/assets/icons/long_arrow_right.svg?react';
 
 export function PokemonEvolution({ evolutions, color }: PokemonEvolutionProps) {
   return (
     <div className="pokemon-evolutions">
-      {evolutions.map((evolution, index) => (
+      {evolutions.map((evolution) => (
         <Fragment key={evolution.name}>
           <div className="pokemon-evolutions__item">
             <div className="pokemon-evolutions__image-container">
-            <LongArrowRight stroke={generateStyleToken('color', color)} />
               <img src={evolution.image} className="pokemon-evolutions__image-item" />
-              {index + 1 < evolutions.length && (
-                <LongArrowRight stroke={generateStyleToken('color', color)} />
-              )}
+              <LongArrowRight
+                className="pokemon-evolutions__separator"
+                stroke={generateStyleToken('color', color)}
+              />
             </div>
-            <Typography color="text-strong" fontSize="2xl" lineHeight="2xl">
+            <Typography
+              className="pokemon-evolutions__label"
+              color="text-strong"
+              fontSize="2xl"
+              lineHeight="2xl"
+            >
               {evolution.name}
             </Typography>
           </div>
